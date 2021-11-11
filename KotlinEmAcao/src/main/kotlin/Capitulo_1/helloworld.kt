244efda4f8eb9aceccbe6b20df57ddc597775fd8
FelipePelegrino <felipe.pelegrino99@gmail.com>
2021-11-11 10:59:34 -0300

    Projeto prático upload

---
+++ KotlinEmAcao/src/main/kotlin/Capitulo_1/helloworld.kt
@@ -0,0 +1,9 @@
+/*
+* Capítulo 1 é mais descritivo e informativo no ambito geral, com exemplos de código para familiarizar o leitor
+* Terá mais anotações no arquivo.txt
+* */
+
+//Um Hello World é sempre bem vindo
+fun main () {
+    println("Hello World")
+}
